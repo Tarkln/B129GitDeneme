925802224814cda870afa527a337cea76615b9e2
Tuğba Arıkanlı <tubaarkln1234@gmail.com>
2023-02-25 18:44:59 +0300

    v2

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -1,5 +1,9 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("merhaba");
+        System.out.println("Dünya");
     }
 }
+
+//Restore --> Tamamen Working space'i temizler
+//Stash__Hafızaya alır
